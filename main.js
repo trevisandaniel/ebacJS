755fb6d96346fa1ea28d58b4bf79d42d4ff4da45
Daniel Trevisan <trevisandaniel2@gmail.com>
2023-04-02 11:16:12 -0300

    Incluindo o código javascript

--- main.js
+++ main.js
@@ -1,1 +1,18 @@
-alert("olá");
+const enviar = document.getElementById('formulario');
+
+function calcular(){
+    const a = document.getElementById('valorA').value;
+    const b = document.getElementById('valorB').value;
+
+    if (a > b){
+        alert('FALHA!! O valor de B é menor do que A!');
+    } else if( a == b){
+        alert('FALHA!! os valore são iguais!');
+    } else{
+        alert('SUCESSO!! O valor de B é maior do que A!');
+    }
+};
+
+enviar.addEventListener('submit', function(e){
+    e.preventDefault();
+})
